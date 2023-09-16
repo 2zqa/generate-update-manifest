337688e4e6a32d3cfa0b07832c890e8f899001ae
2zqa <25235249+2zqa@users.noreply.github.com>
2023-09-16 02:16:23 +0200

    Implement generateUpdateManifest

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,8 @@
 import * as core from '@actions/core'
 import * as github from '@actions/github'
 import GitHubRelease from './types/GitHubRelease'
-import UpdateManifest from './types/UpdateManifest'
+import UpdateManifest, { Update } from './types/UpdateManifest'
+import * as fs from 'node:fs/promises'
 
 /**
  * The main function for the action.
@@ -11,7 +12,6 @@ export async function run(): Promise<void> {
   try {
     const token = core.getInput('github-token')
     const addonId = core.getInput('addon-id')
-
     const client = github.getOctokit(token)
 
     const releases = await client.request(
@@ -19,7 +19,7 @@ export async function run(): Promise<void> {
       github.context.repo
     )
     const manifest = generateUpdateManifest(releases.data, addonId)
-    core.info(JSON.stringify(manifest))
+    return fs.writeFile('manifest.json', JSON.stringify(manifest, null, 2))
   } catch (error) {
     // Fail the workflow run if an error occurs
     if (error instanceof Error) core.setFailed(error.message)
@@ -30,7 +30,16 @@ export function generateUpdateManifest(
   releases: GitHubRelease[],
   addonId: string
 ): UpdateManifest {
-  core.debug(releases.join(':'))
-  core.debug(addonId)
-  throw new Error('Function not implemented.')
+  const mappedUpdates: Update[] = releases.map(release => ({
+    version: release.tag_name,
+    update_link: release.assets[0]?.browser_download_url
+  }))
+
+  return {
+    addons: {
+      [addonId]: {
+        updates: mappedUpdates
+      }
+    }
+  }
 }
