e138bae267be5f2a94c80d07eb9347b821e1c183
2zqa <25235249+2zqa@users.noreply.github.com>
2023-09-20 15:21:12 +0200

    Add test for validateAddonID

--- __tests__/main.test.ts
+++ __tests__/main.test.ts
@@ -6,6 +6,7 @@ import * as fs from 'node:fs/promises'
 import Validator from '../src/validator'
 
 const addonIDRegex = '{692884b1-e357-4adb-9e3c-f4a3d74bb38b}'
+const addonIDEmail = 'uBlock0@raymondhill.net'
 
 describe('generateUpdateManifest', () => {
   it('returns a manifest with an empty updates list', () => {
@@ -35,5 +36,28 @@ describe('generateUpdateManifest', () => {
     const manifest = main.generateUpdateManifest(releases, addonIDRegex)
 
     expect(manifest).toEqual(expected)
+  })
+})
+
+describe('validateAddonID', () => {
+  let validator: Validator
+
+  beforeEach(() => {
+    validator = new Validator()
+  })
+
+  it('does not add an error for a valid addon ID in UUID form', () => {
+    main.validateAddonID(validator, addonIDRegex)
+    expect(validator.isValid()).toBe(true)
+  })
+
+  it('does not add an error for a valid addon ID in e-mail form', () => {
+    main.validateAddonID(validator, addonIDEmail)
+    expect(validator.isValid()).toBe(true)
+  })
+
+  it('adds an error for an invalid addon ID', () => {
+    main.validateAddonID(validator, 'not-an-addon-id')
+    expect(validator.isValid()).toBe(false)
   })
 })
