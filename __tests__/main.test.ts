6ba675fedfeaf0b23569e1b7f64d98122eb8cb02
2zqa <25235249+2zqa@users.noreply.github.com>
2023-09-20 10:20:12 +0200

    Rename testUuid, use promise fs

--- __tests__/main.test.ts
+++ __tests__/main.test.ts
@@ -2,28 +2,37 @@
  * Unit tests for the action's main business logic, src/main.ts
  */
 import * as main from '../src/main'
-import fs from 'fs'
+import * as fs from 'node:fs/promises'
+import Validator from '../src/validator'
 
-const testUuid = '{692884b1-e357-4adb-9e3c-f4a3d74bb38b}'
+const addonID = '{692884b1-e357-4adb-9e3c-f4a3d74bb38b}'
 
 describe('generateUpdateManifest', () => {
   it('returns a manifest with an empty updates list', () => {
-    expect(main.generateUpdateManifest([], testUuid)).toEqual({
+    expect(main.generateUpdateManifest([], addonID)).toEqual({
       addons: {
-        [testUuid]: {
+        [addonID]: {
           updates: []
         }
       }
     })
   })
 
-  it('generates a manifest', () => {
-    const expectedJson = fs.readFileSync('./__tests__/fixtures/expected.json')
-    const mockedResponse = fs.readFileSync('./__tests__/fixtures/releases.json')
+  it('generates a manifest', async () => {
+    const expectedJsonPromise = fs.readFile(
+      './__tests__/fixtures/expected.json'
+    )
+    const mockedResponsePromise = fs.readFile(
+      './__tests__/fixtures/releases.json'
+    )
+    const [expectedJson, mockedResponse] = await Promise.all([
+      expectedJsonPromise,
+      mockedResponsePromise
+    ])
     const expected = JSON.parse(expectedJson.toString())
     const releases = JSON.parse(mockedResponse.toString())
 
-    const manifest = main.generateUpdateManifest(releases, testUuid)
+    const manifest = main.generateUpdateManifest(releases, addonID)
 
     expect(manifest).toEqual(expected)
   })
